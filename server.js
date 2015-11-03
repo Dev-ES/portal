1333c8231f828caf27a456e1c5936a3e601ee7f3
Marcos Dioni Lima <madili2016@gmail.com>
2015-11-03 15:17:17 -0200

    config port

--- server.js
+++ server.js
@@ -1,6 +1,6 @@
 var http = require('http');
 var app = require('./server/express')();
 
-http.createServer(app).listen(process.env.IP, process.env.ADDRESS, function() {
+http.createServer(app).listen(process.env.PORT, process.env.IP, function() {
 	console.log('Server On!');
 });
