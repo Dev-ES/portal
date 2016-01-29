0d4c27d0d12ed1e1df0a03962d041132afac3957
Marcos Dioni Lima <mlima@vix.itextreme.com.br>
2016-01-29 15:15:42 -0200

    include files

--- server.js
+++ server.js
@@ -1,6 +1,6 @@
 var http = require('http');
 var app = require('./server/express')();
 
-http.createServer(app).listen(process.env.PORT, process.env.IP, function() {
+http.createServer(app).listen(process.env.PORT || 3000, process.env.IP || 'localhost', function() {
 	console.log('Server On!');
 });
